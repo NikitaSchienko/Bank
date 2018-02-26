a0a9afd17ad7e4f612e411937163a07eae259168
nikita <audi.087@yandex.ru>
2018-02-26 16:14:11 +0400

    Add logging

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
+import org.apache.log4j.Logger;
+
 public class Main {
+
+    final static Logger logger = Logger.getLogger(Main.class);
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        String parameter = "test";
+        if(logger.isDebugEnabled()){
+            logger.debug("This is debug : " + parameter);
+        }
+
+        if(logger.isInfoEnabled()){
+            logger.info("This is info : " + parameter);
+        }
+
+        logger.warn("This is warn : " + parameter);
+        logger.error("This is error : " + parameter);
+        logger.fatal("This is fatal : " + parameter);
     }
 }
